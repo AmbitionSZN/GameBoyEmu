a48cee34ece175eacdf7e3384f91c425b0d42f87
AmbitionSZN <jahlwalk22635@gmail.com>
2026-01-09 15:29:41 -0500

    lots of json parsing

--- src/cpu.c
+++ src/cpu.c
@@ -1,31 +1,225 @@
 #include "cpu.h"
 #include "bus.h"
+#include "cJSON.h"
 #include "cart.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 Instruction Instructions[512];
 
+DataType GetOperandType(cJSON *operand, char *mnemonic) {
+    char *op = operand->child->valuestring;
+    if (strcmp(op, "A") == 0) {
+        return DT_A;
+    }
+    if (strcmp(op, "F") == 0) {
+        return DT_F;
+    }
+    if (strcmp(op, "B") == 0) {
+        return DT_B;
+    }
+    if (strcmp(op, "C") == 0) {
+        if (strcmp(mnemonic, "CALL") == 0 || strcmp(mnemonic, "JP") == 0 ||
+            strcmp(mnemonic, "JR") == 0 || strcmp(mnemonic, "RET") == 0) {
+            return DT_CC_C;
+        }
+        return DT_C;
+    }
+    if (strcmp(op, "D") == 0) {
+        return DT_D;
+    }
+    if (strcmp(op, "E") == 0) {
+        return DT_E;
+    }
+    if (strcmp(op, "H") == 0) {
+        return DT_H;
+    }
+    if (strcmp(op, "L") == 0) {
+        return DT_L;
+    }
+    if (strcmp(op, "AF") == 0) {
+        if (cJSON_IsTrue(operand->child->next)) {
+            return DT_AF;
+        }
+        return DT_A_AF;
+    }
+    if (strcmp(op, "BC") == 0) {
+        if (cJSON_IsTrue(operand->child->next)) {
+            return DT_BC;
+        }
+        return DT_A_BC;
+    }
+    if (strcmp(op, "DE") == 0) {
+        if (cJSON_IsTrue(operand->child->next)) {
+            return DT_DE;
+        }
+        return DT_A_DE;
+    }
+    if (strcmp(op, "HL") == 0) {
+        bool immediate;
+        if (strcmp(operand->child->next->string, "increment") == 0) {
+            if (cJSON_IsTrue(operand->child->next)) {
+                return DT_HLI;
+            }
+            return DT_HLD;
+        }
+        immediate = cJSON_IsTrue(operand->child->next);
+        if (immediate) {
+            return DT_HL;
+        }
+        return DT_A_HL;
+    }
+
+    if (strcmp(op, "SP") == 0) {
+        return DT_SP;
+    }
+    if (strcmp(op, "PC") == 0) {
+        return DT_PC;
+    }
+    if (strcmp(op, "n8") == 0) {
+        return DT_N8;
+    }
+    if (strcmp(op, "n16") == 0) {
+        return DT_N16;
+    }
+    if (strcmp(op, "e8") == 0) {
+        return DT_E8;
+    }
+    if (strcmp(op, "Z") == 0) {
+        return DT_CC_Z;
+    }
+    if (strcmp(op, "NZ") == 0) {
+        return DT_CC_NZ;
+    }
+    if (strcmp(op, "NC") == 0) {
+        return DT_CC_NC;
+    }
+    if (strcmp(op, "$08") == 0) {
+        return DT_RST8;
+    }
+    if (strcmp(op, "$18") == 0) {
+        return DT_RST18;
+    }
+    if (strcmp(op, "$28") == 0) {
+        return DT_RST28;
+    }
+    if (strcmp(op, "$38") == 0) {
+        return DT_RST38;
+    }
+    if (strcmp(op, "a8") == 0) {
+        return DT_A8;
+    }
+    if (strcmp(op, "a16") == 0) {
+        return DT_A16;
+    }
+    return DT_NONE;
+}
+
 void opcodesJsonParser(char *file) {
     FILE *fptr;
-	char *json;
-	size_t jsonSize;
+    char *str;
+    size_t strSize;
     fptr = fopen(file, "rb");
     if (fptr == NULL) {
-        printf("failed to open rom: %s", file);
+        printf("failed to open json file: %s", file);
         exit(EXIT_FAILURE);
     }
     fseek(fptr, 0, SEEK_END);
-    jsonSize = ftell(fptr);
+    strSize = ftell(fptr);
     rewind(fptr);
-    json = malloc(jsonSize);
-    fread(json, jsonSize, 1, fptr);
+    str = malloc(strSize);
+    fread(str, strSize, 1, fptr);
     fclose(fptr);
-
-	for (size_t i = 0; i < 100; i++) {
-		printf("%s", &json[i]);
-	}
+    cJSON *json = cJSON_Parse(str);
+    cJSON *unprefixed = json->child;
+    cJSON *opcode = NULL;
+    printf("%i\n", cJSON_GetArraySize(unprefixed));
+    for (size_t i = 0; i < cJSON_GetArraySize(unprefixed); i++) {
+        Instruction instruction;
+        if (!opcode) {
+            opcode = unprefixed->child;
+        } else {
+            opcode = opcode->next;
+        }
+        instruction.Mnemonic =
+            cJSON_GetObjectItem(opcode, "mnemonic")->valuestring;
+        instruction.Bytes = cJSON_GetObjectItem(opcode, "bytes")->valueint;
+        cJSON *jsonCycles = cJSON_GetObjectItem(opcode, "cycles");
+        if (cJSON_GetArraySize(jsonCycles) == 2) {
+            instruction.Cycles[0] = jsonCycles->child->valueint / 4;
+            instruction.Cycles[1] = jsonCycles->child->next->valueint / 4;
+        } else {
+            instruction.Cycles[0] = jsonCycles->child->valueint / 4;
+            instruction.Cycles[1] = 0;
+        }
+        cJSON *jsonOperands = cJSON_GetObjectItem(opcode, "operands");
+        size_t operSize = cJSON_GetArraySize(jsonOperands);
+        if (operSize == 0) {
+            instruction.Operand1 = DT_NONE;
+            instruction.Operand2 = DT_NONE;
+        }
+        if (operSize == 1) {
+            cJSON *op1 = jsonOperands->child;
+            instruction.Operand1 =
+                GetOperandType(op1, instruction.Mnemonic);
+            instruction.Operand2 = DT_NONE;
+        }
+        if (operSize == 2) {
+            cJSON *op1 = jsonOperands->child;
+            cJSON *op2 = jsonOperands->child->next;
+            instruction.Operand1 =
+                GetOperandType(op1, instruction.Mnemonic);
+            instruction.Operand2 =
+                GetOperandType(op2, instruction.Mnemonic);
+        }
+        cJSON *jsonFlags = cJSON_GetObjectItem(opcode, "flags");
+        cJSON *jsonFlag = NULL;
+        for (size_t j = 0; j < 4; j++) {
+            if (!jsonFlag) {
+                jsonFlag = jsonFlags->child;
+            } else {
+                jsonFlag = jsonFlag->next;
+            }
+            if (cJSON_IsString(jsonFlag) == false && jsonFlag->valueint == 0) {
+                instruction.Flags[j] = FLAG_CLEAR;
+                continue;
+            }
+            if (cJSON_IsString(jsonFlag) == false && jsonFlag->valueint == 1) {
+                instruction.Flags[j] = FLAG_SET;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == '0') {
+                instruction.Flags[j] = FLAG_CLEAR;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == '1') {
+                instruction.Flags[j] = FLAG_SET;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == '-') {
+                instruction.Flags[j] = FLAG_NONE;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == 'Z') {
+                instruction.Flags[j] = FLAG_Z;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == 'N') {
+                instruction.Flags[j] = FLAG_N;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == 'H') {
+                instruction.Flags[j] = FLAG_H;
+                continue;
+            }
+            if (jsonFlag->valuestring[0] == 'C') {
+                instruction.Flags[j] = FLAG_C;
+                continue;
+            }
+        }
+    }
 };
 
 void jump(CPU *cpu, Cartridge cart) {
--- src/cpu.h
+++ src/cpu.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "cart.h"
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+
 typedef struct {
     uint8_t A;
     uint8_t F;
@@ -17,10 +18,39 @@ typedef struct {
 } CPURegisters;
 
 typedef enum {
-	NONE,
-	SET_FLAG,
-	ZERO_FLAG,
-} FlagInstructions;
+    AM_IMP,
+    AM_R_N16,
+    AM_R_R,
+    AM_MR_R,
+    AM_R,
+    AM_R_N8,
+    AM_R_MR,
+    AM_R_HLI,
+    AM_R_HLD,
+    AM_HLI_R,
+    AM_HLD_R,
+    AM_R_A8,
+    AM_A8_R,
+    AM_HL_SPR,
+    AM_N16,
+    AM_N8,
+    AM_N16_R,
+    AM_MR_N8,
+    AM_MR,
+    AM_A16_R,
+    AM_R_A16,
+    AM_E8_
+} AddressingMode;
+
+typedef enum {
+    FLAG_NONE,
+    FLAG_CLEAR,
+    FLAG_SET,
+    FLAG_Z,
+    FLAG_N,
+    FLAG_H,
+    FLAG_C,
+} FlagInstruction;
 
 typedef enum {
     IN_NONE,
@@ -60,37 +90,105 @@ typedef enum {
     IN_EI,
     IN_RST,
     IN_ERR,
-    //CB instructions...
-    IN_RLC, 
+    // CB instructions...
+    IN_RLC,
     IN_RRC,
-    IN_RL, 
+    IN_RL,
     IN_RR,
-    IN_SLA, 
+    IN_SLA,
     IN_SRA,
-    IN_SWAP, 
+    IN_SWAP,
     IN_SRL,
-    IN_BIT, 
-    IN_RES, 
+    IN_BIT,
+    IN_RES,
     IN_SET
-} Mnemonics;
+} Mnemonic;
 
-void opcodesJsonParser(char* file);
+typedef enum {
+    RT_NONE,
+    RT_A,
+    RT_F,
+    RT_B,
+    RT_C,
+    RT_D,
+    RT_E,
+    RT_H,
+    RT_L,
+    RT_AF,
+    RT_BC,
+    RT_DE,
+    RT_HL,
+    RT_SP,
+    RT_PC
+} RegType;
 
+typedef enum {
+    DT_NONE,
+    DT_A,
+    DT_F,
+    DT_B,
+    DT_C,
+    DT_D,
+    DT_E,
+    DT_H,
+    DT_L,
+    DT_AF,
+    DT_BC,
+    DT_DE,
+    DT_HL,
+	DT_HLI,
+	DT_HLD,
+    DT_SP,
+    DT_PC,
+    DT_N8,
+    DT_N16,
+    DT_E8,
+	DT_CC_Z,
+	DT_CC_C,
+	DT_CC_NZ,
+	DT_CC_NC,
+	DT_RST8,
+	DT_RST18,
+	DT_RST28,
+	DT_RST38,
+	DT_A8,
+	DT_A16,
+	DT_A_AF,
+	DT_A_BC,
+	DT_A_DE,
+	DT_A_HL,
+	DT_A_HLI,
+	DT_A_HLD,
+} DataType;
+
+typedef enum {
+    RST_VEC0 = 0x00,
+    RST_VEC8 = 0x08,
+    RST_VEC10 = 0x10,
+    RST_VEC18 = 0x18,
+    RST_VEC20 = 0x20,
+    RST_VEC28 = 0x28,
+    RST_VEC30 = 0x30,
+    RST_VEC38 = 0x38
+} RSTVec;
+
+void opcodesJsonParser(char *file);
 
 typedef struct {
-	uint8_t Mnemonic;
-	uint8_t Bytes;
-	uint8_t cycles;
-	uint8_t Flags[4];
-	bool Immediate;
+    char *Mnemonic;
+    uint8_t Bytes;
+    uint8_t Cycles[2];
+	DataType Operand1;
+	DataType Operand2;
+    FlagInstruction Flags[4];
+    bool Immediate;
+    AddressingMode AddrMode;
 } Instruction;
 
-
 typedef struct {
-	CPURegisters Regs;
-	Instruction CurInstr;
+    CPURegisters Regs;
+    Instruction CurInstr;
 } CPU;
 
-void fetchInstruction(CPU* cpu, Cartridge cart);
-void execute(CPU* cpu);
-
+void fetchInstruction(CPU *cpu, Cartridge cart);
+void execute(CPU *cpu);
