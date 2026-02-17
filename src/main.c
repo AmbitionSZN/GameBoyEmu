e9f07acdf1243518070938109cb0fd5cd1ef0a2d
AmbitionSZN <jahlwalk22635@gmail.com>
2026-02-17 14:13:40 -0500

    cleaned up main a bit

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "emu.h"
 #include <SDL3/SDL_video.h>
-#include <stdlib.h>
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
-#include "bus.h"
 #include "cart.h"
 #include "cpu.h"
 #include <SDL3/SDL.h>
@@ -20,7 +18,6 @@ FILE *logFile;
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Example Renderer Clear", "1.0",
                        "com.example.renderer-clear");
@@ -38,14 +35,13 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetRenderLogicalPresentation(renderer, 640, 480,
                                      SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-
     logFile = fopen("../logs/log.txt", "w");
 
     cart = LoadCartridge("../roms/09-op r,r.gb");
     opcodesJsonParser("../Opcodes.json");
     cpuInit();
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
@@ -54,32 +50,29 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
         return SDL_APP_SUCCESS; /* end the program, reporting success to the OS.
                                  */
     }
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    const double now = ((double)SDL_GetTicks()) /
-                       1000.0; /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it
-     * fade between colors smoothly. */
-    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
-    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(
-        renderer, red, green, blue,
-        SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
-    for (size_t i = 0; i < 10; ++i) {
+    const double targetFrameTime = 1.0 / 60.0;
+    const double safetyMargin = 0.002;        // 2ms margin for rendering overhead
+    const double startTime = ((double)SDL_GetTicks()) / 1000.0;
+    
+    // Run cpuStep as many times as possible until we hit our time limit
+    while ((((double)SDL_GetTicks()) / 1000.0) - startTime < (targetFrameTime - safetyMargin)) {
         cpuStep();
     }
 
-    /* clear the window to the draw color. */
-    SDL_RenderClear(renderer);
+    const double now = ((double)SDL_GetTicks()) / 1000.0;
+    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
 
-    /* put the newly-cleared rendering on the screen. */
+    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
 /* This function runs once at shutdown. */
